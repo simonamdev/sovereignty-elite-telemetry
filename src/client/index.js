e4289b466fc9d267f6eeb2c3760da79e7c3080ac
Simon AM <simon@agius-muscat.net>
2018-04-07 10:49:34 +0200

    Show different pads using triangles

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import Service from './service';
 import * as PIXI from 'pixi.js'
 
+import * as triangleImage from './images/triangle.png';
 import * as viperImage from './images/test-viper.png';
 import * as arenaImage from './images/isola1-small.jpg';
 
@@ -8,12 +9,17 @@ import { degreesToRadians, lonLatToXY } from './conversions';
 
 import './scss/index.scss';
 
+// Currently hardcoded at 1920x1080, but ideally we get the width of the window
+const width = 1920;
+const height = 1080;
+
 const testUrl = 'http://127.0.0.1:5000/';
-const imageSideSize = 70;
-const imageScale = 5.5;
-const imageOffsetAngle = 75;
-const imageX = 900;
-const imageY = -750;
+const shipImageSideSize = 70;
+const imageScale = 1;
+const imageOffsetAngle = 0;
+const imageX = 960;
+const imageY = 540;
+// const imageY = window.innerHeight / 2;
 
 const headingOffset = 0; // Heading does not point directly up in the image
 // const headingOffset = 116; // Heading does not point directly up in the image
@@ -21,6 +27,11 @@ const originLon = 43.460500;
 const originLat = -64.534150;
 // const originLon = 42.622185;
 // const originLat = -64.288918;
+
+// Centre of image co-ordinates:
+const centreLon = 43.54425;
+const centreLat = -64.459335;
+const centreHeading = 65;
 
 // Planet details
 // Pad numbers are LEFT TO RIGHT not actual pad numbers
@@ -34,6 +45,57 @@ const originLat = -64.534150;
 // Pad 6: 43.478378, -64.564034, 113
 // Pad 7: 43.623196, -64.590424, 284
 // Pad 8: 43.598091, -64.613457, 289
+
+let pads = [
+    {
+        'number': 1,
+        'lon': 43.321926,
+        'lat': -64.391991,
+        'heading': 225
+    },
+    {
+        'number': 2,
+        'lon': 43.321926,
+        'lat': -64.391991,
+        'heading': 225
+    },
+    {
+        'number': 3,
+        'lon': 43.261738,
+        'lat': -64.440659,
+        'heading': 31
+    },
+    {
+        'number': 4,
+        'lon': 43.269848,
+        'lat': -64.437202,
+        'heading': 49
+    },
+    {
+        'number': 5,
+        'lon': 43.504578,
+        'lat': -64.527588,
+        'heading': 114
+    },
+    {
+        'number': 6,
+        'lon': 43.478378,
+        'lat': -64.564034,
+        'heading': 113
+    },
+    {
+        'number': 7,
+        'lon': 43.623196,
+        'lat': -64.590424,
+        'heading': 284
+    },
+    {
+        'number': 8,
+        'lon': 43.598091,
+        'lat': -64.613457,
+        'heading': 289
+    }
+];
 
 
 console.log('Setting up comms');
@@ -65,9 +127,6 @@ wsService.socket.on('latencyResponse', (response) => {
 wsService.socket.on('overlayPositionUpdate', (response) => {
     console.log(`Position Update Response: ${JSON.stringify(response)}`);
     // Values need to be mapped
-    // Currently hardcoded at 1920x1080, but ideally we get the width of the window
-    const width = 1920;
-    const height = 1080;
     let position = lonLatToXY(originLon, originLat, response.lon, response.lat);
     lon = response.lon;
     lat = response.lat;
@@ -126,15 +185,15 @@ let texture = new PIXI.Texture(viperTex);
 PIXI.Texture.addToCache(texture, 'viper');
 let viper = PIXI.Sprite.fromImage('viper');
 
-viper.width = imageSideSize;
-viper.height = imageSideSize;
+viper.width = shipImageSideSize;
+viper.height = shipImageSideSize;
 
 // Set the rotation anchor point by the center
 viper.anchor.x = 0.5;
 viper.anchor.y = 0.5;
 
 // Add the viper
-app.stage.addChild(viper);
+// app.stage.addChild(viper); // Do not add the viper for now
 
 debugInfo = `Lon: ${lon},
 Lat: ${lat},
@@ -151,7 +210,32 @@ let debugText = new PIXI.Text(
         fill: '#ffffff'
     }
 );
-app.stage.addChild(debugText);
+// app.stage.addChild(debugText); // Do not add the text
+
+let image = new Image();
+image.src = triangleImage;
+let triTex = new PIXI.BaseTexture(image);
+let texture = new PIXI.Texture(triTex);
+PIXI.Texture.addToCache(texture, 'triangle');
+// Add the pad triangles
+for (let i = 0; i < pads.length; i++) {
+    let pad = pads[i];
+    console.log(pad);
+    let triangle = PIXI.Sprite.fromImage('triangle');
+    let position = lonLatToXY(originLon, originLat, pad.lon, pad.lat);
+    // let position = lonLatToXY(centreLon, centreLat, pad.lon, pad.lat);
+    let x = position.x * width;
+    let y = position.y * height;
+    triangle.x = x;
+    triangle.y = y;
+    triangle.anchor.x = 0.5;
+    triangle.anchor.y = 0.5;
+    const triangleScale = 0.1;
+    triangle.scale.x = triangleScale;
+    triangle.scale.y = triangleScale;
+    triangle.rotation = degreesToRadians(pad['heading']);
+    app.stage.addChild(triangle); // Do not add the text
+}
 
 // Start the loop
 let gameLoop = (delta) => {
@@ -177,7 +261,7 @@ app.ticker.add(delta => gameLoop(delta));
 // Resize to fit the window
 app.renderer.view.style.position = 'absolute';
 app.renderer.view.style.display = 'block';
-// app.renderer.autoResize = true;
-// app.renderer.resize(window.innerWidth, window.innerHeight);
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(app.view);
